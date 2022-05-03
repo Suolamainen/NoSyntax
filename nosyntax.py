da216451edc03e6c1a001b1bc3746bc33a809ea7
Suolamainen <93546777+Suolamainen@users.noreply.github.com>
2022-05-03 12:12:33 +0300

    Create nosyntax.py

---
+++ nosyntax.py
@@ -0,0 +1,11 @@
+if 1 < 2:
+    print"(Hello world")
+else
+print("Hello"):
+    if Hello world>2"1"
+    prints(f"True * {2}")
+    elif(printing(5*2))
+    elseprint("True"):
+    #code
+    prints("This works if ")
+else: print("Hello wrodl")
